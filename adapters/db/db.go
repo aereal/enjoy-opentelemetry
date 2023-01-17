f908ade058834b33d78ed9ef0036bba7519aec5d
aereal <aereal@aereal.org>
2023-01-17 22:33:26 +0900

    fix: set default parameters

--- adapters/db/db.go
+++ adapters/db/db.go
@@ -2,15 +2,32 @@ package db
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/XSAM/otelsql"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"go.opentelemetry.io/otel/trace"
 )
 
+var defaultLoc *time.Location
+
+func init() {
+	var err error
+	defaultLoc, err = time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func New(tp trace.TracerProvider, dsn string) (*sqlx.DB, error) {
-	db, err := otelsql.Open("mysql", dsn, otelsql.WithTracerProvider(tp), otelsql.WithSpanOptions(otelsql.SpanOptions{DisableErrSkip: true}))
+	cfg, err := mysql.ParseDSN(dsn)
+	if err != nil {
+		return nil, err
+	}
+	cfg.ParseTime = true
+	cfg.Loc = defaultLoc
+	db, err := otelsql.Open("mysql", cfg.FormatDSN(), otelsql.WithTracerProvider(tp), otelsql.WithSpanOptions(otelsql.SpanOptions{DisableErrSkip: true}))
 	if err != nil {
 		return nil, fmt.Errorf("otelsql.Open: %w", err)
 	}
