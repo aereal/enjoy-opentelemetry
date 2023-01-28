23b96601257e528f059c572e7e77d28f26a456c0
aereal <aereal@aereal.org>
2023-01-28 22:10:29 +0900

    feat: import script

--- db/ddl.sql
+++ db/ddl.sql
@@ -5,3 +5,16 @@ create table if not exists livers (
   `debuted_on` date not null,
   `retired_on` date
 ) engine=InnoDB default character set=utf8mb4;
+
+drop table if exists `liver_groups`;
+create table if not exists `liver_groups` (
+  `group_id` bigint unsigned not null auto_increment primary key,
+  `name` varchar(255) not null unique key
+) engine=InnoDB default charset=utf8mb4;
+
+drop table if exists `liver_group_members`;
+create table if not exists `liver_group_members` (
+  `liver_group_id` bigint unsigned not null,
+  `liver_id` bigint signed not null,
+  primary key (`liver_group_id`, `liver_id`)
+) engine=InnoDB default charset=utf8mb4;
